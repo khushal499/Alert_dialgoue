91659d0b7686d8c01450fcb3b209519ab6a7efb6
khushal499 <201b134@juetguna.in>
2022-06-01 16:32:08 +0530

    alert

---
+++ app/src/main/java/com/example/alert_dialgoue/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.alert_dialgoue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.ic_k)
+                .setTitle("be Alert!")
+                .setMessage("Follow social distancing")
+                .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
